054a280ba5c439af01ddcbcc7cc8fb8d0c5e4e66
Osifali <jakub.kandricak@gmail.com>
2019-03-04 22:36:07 +0100

    git commit

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -3,32 +3,35 @@
 # __Commit__ it to your repository into `Lessons/Lesson2/Homework`.
 
 ## Code
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+library(dplyr)
+
+library(ggplot2)
+
+dt_KPI <- read.csv("C:\\Users\\Jakub\\Documents\\GeneralInsurance_Class\\Data\\lesson2_KPI.csv")
+
+data1 <- dt_KPI %>% 
+  mutate(Profit = Premium - Losses - Expenses)
+
+data1 %>% 
+  group_by(Unit) %>% 
+  summarise(Prof= sum(Profit,na.rm=T)) %>% 
+  arrange(Prof,Unit)
+
+
+data1 %>%
+  filter(Unit == "Unit7") %>% 
+  group_by(Year) %>%
+  summarise(Profyear = sum(Profit,na.rm=T)) %>% 
+  arrange(Profyear,Year) %>% 
+  ggplot(mapping = aes(x = Year, y = Profyear))+
+  geom_bar(stat="identity")
+
+
 
 
 
 
 # Your Explanation about analysis:
-# 
-# 
-# 
+# 1. Zistil som portfolio s najvacsim profitom (bral som ako portfolio Unit) -> Unit7
+# 2. Pre Unit 7 som zistil najhorsi rok -> 2014
+# 3. Podla grafu je najhorší rok 2014 v najlepšom portfóliu Unit7, čo mozeme vidiet aj, ked si vypiseme roky s profitmi.
