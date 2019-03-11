a157f320461e06653a01b431ae69697f5361ca39
Osifali <jakub.kandricak@gmail.com>
2019-03-11 23:33:34 +0100

    DU3

--- Lessons/Lesson3/Homework/Shiny/ui.R
+++ Lessons/Lesson3/Homework/Shiny/ui.R
@@ -1,37 +1,22 @@
+library(ggplot2)
+library(shiny)
+library(dplyr)
+dt_KPI1 <- read.csv("C:\\Users\\Jakub\\Documents\\GeneralInsurance_Class\\Data\\lesson2_KPI.csv")
+dt_KPI <- dt_KPI1 %>% filter_all(all_vars(!is.na(.)))
+ui <- fluidPage(    
 
-
-#### MAIN PAGE ####
-ui <- fluidPage(theme = shinytheme("paper"),
-                title = "General Insurance Class",
-                navbarPage(
-                  title = span(
-                    img(src = "logo_zurich.png", height = 30),
-                    "General Insurance Class"
-                  ),
-                  tabPanel("lesson3",
-                           tagList(
-                             fluidRow(div(
-                               style = "text-align:center", h3("This is second class about KPIs")
-                             )),
-                             navlistPanel(widths = c(2, 6),
-                                          tabPanel(
-                                            "Focus Group",
-                                            fluidRow(column(
-                                              width = 3,
-                                              offset = 9,
-                                              uiOutput("lesson3_KPI_multidim_select_axis_render")
-                                            )),
-                                            fluidRow(
-                                              column(width = 9,
-                                                     DT::dataTableOutput("lesson3_KPI_multidim_table")),
-                                              column(
-                                                width = 3,
-                                                plotOutput("lesson3_KPI_multidim_ratio_graph",
-                                                           width = 400),
-                                                plotOutput("lesson3_KPI_multidim_UWR_graph",
-                                                           width = 400)
-                                              )
-                                            )
-                                          ))
-                           ))
-                ))
+  titlePanel("Závislosť Premium a Expenses"),
+  
+  sidebarLayout(      
+    
+    sidebarPanel(
+      selectInput("vyber", "Výber:", 
+                  choices=colnames(dt_KPI[,1:5]))
+    ),
+    
+    mainPanel(
+      plotOutput("ggplot")  
+    )
+    
+  )
+)
